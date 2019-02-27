890dcbfe65e16ddd0e404f55f999a1abf6f50782
Felipe Panegalli <felipe.panegalli@gmail.com>
2019-02-27 07:53:21 -0300

    Finalizado exercício 3

--- mod01/desafios/exercicio03-bundle.js
+++ mod01/desafios/exercicio03-bundle.js
@@ -12,23 +12,30 @@ var usuario = {
   idade: 23
 };
 
-function mostraIdade(usuario) {
+var mostraIdade = function mostraIdade(usuario) {
   return usuario.idade;
-}
+};
 
-console.log(mostraIdade(usuario)); // Dica: Utilize uma constante pra function
+console.log(mostraIdade(usuario)); // 3.3
+// Dica: Utilize uma constante pra function
 
 var nome = 'Diego';
 var idade = 23;
 
-function mostraUsuario() {
+var mostraUsuario = function mostraUsuario() {
   var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
   var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
   return {
     nome: nome,
     idade: idade
   };
-}
+};
 
-mostraUsuario(nome, idade);
-mostraUsuario(nome);
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); // 3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
+  });
+};
--- mod01/desafios/exercicio03.js
+++ mod01/desafios/exercicio03.js
@@ -5,16 +5,16 @@ console.log(arr.map((item) => item + 10));
 // 3.2
 // Dica: Utilize uma constante pra function
 const usuario = { nome: 'Diego', idade: 23 };
-function mostraIdade(usuario) {
-    return usuario.idade;
-}
+const mostraIdade = (usuario) => usuario.idade;
 console.log(mostraIdade(usuario));
 
+// 3.3
 // Dica: Utilize uma constante pra function
 const nome = 'Diego';
 const idade = 23;
-function mostraUsuario(nome = 'Diego', idade = 18) {
-    return { nome, idade };
-}
-mostraUsuario(nome, idade);
-mostraUsuario(nome);
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({ nome, idade });
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome));
+
+// 3.4
+const promise = () => new Promise((resolve, reject) => resolve());
